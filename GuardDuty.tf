04051e44bf6b127ee6f5eaffa7df37164e327796
xsuwittch <ammarworksonline@gmail.com>
2026-07-05 12:02:17 +0500

    Added Guard duty target and finding rules

--- GuardDuty.tf
+++ GuardDuty.tf
@@ -13,3 +13,18 @@ resource "aws_sns_topic_subscription" "guardduty_sns_subscription" {
   protocol  = "email"
   endpoint  = "ammarworksonline@gmail.com"
 }
+
+resource "aws_cloudwatch_event_rule" "guardduty_finding_rule" {
+  name        = "guardduty-finding-rule"
+  description = "Capture GuardDuty findings"
+  event_pattern = jsonencode({
+    source      = ["aws.guardduty"]
+    detail-type = ["GuardDuty Finding"]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "guardduty_finding_target" {
+  rule      = aws_cloudwatch_event_rule.guardduty_finding_rule.name
+  target_id = "GuardDutyFindingTarget"
+  arn       = aws_sns_topic.guardduty_sns_topic.arn
+}
