04dd9161cfc67057626b98a94df60cf221804ac8
xsuwittch <ammarworksonline@gmail.com>
2026-07-05 12:05:13 +0500

    Policy allowing EventBridge to use the guardduty sns topic

--- GuardDuty.tf
+++ GuardDuty.tf
@@ -7,12 +7,13 @@ resource "aws_sns_topic" "guardduty_sns_topic" {
   name = "guardduty_sns_topic"
   
 }
-
+// SNS subscription to receive GuardDuty findings
 resource "aws_sns_topic_subscription" "guardduty_sns_subscription" {
   topic_arn = aws_sns_topic.guardduty_sns_topic.arn
   protocol  = "email"
   endpoint  = "ammarworksonline@gmail.com"
 }
+// EventBridge rule to capture GuardDuty findings
 
 resource "aws_cloudwatch_event_rule" "guardduty_finding_rule" {
   name        = "guardduty-finding-rule"
@@ -23,8 +24,27 @@ resource "aws_cloudwatch_event_rule" "guardduty_finding_rule" {
   })
 }
 
+// EventBridge target to send GuardDuty findings to the SNS topic
 resource "aws_cloudwatch_event_target" "guardduty_finding_target" {
   rule      = aws_cloudwatch_event_rule.guardduty_finding_rule.name
   target_id = "GuardDutyFindingTarget"
   arn       = aws_sns_topic.guardduty_sns_topic.arn
+}
+
+
+// policy to allow EventBridge to publish to the SNS topic
+resource "aws_sns_topic_policy" "allow_eventbridge" {
+  arn = aws_sns_topic.guardduty_sns_topic.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "events.amazonaws.com" }
+        Action    = "SNS:Publish"
+        Resource  = aws_sns_topic.guardduty_sns_topic.arn
+      }
+    ]
+  })
 }
